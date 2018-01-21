fa7a983a2635b9945fcc421e713e160f03703f3d
Johanna Honma <johonma@hotmail.com>
2018-01-20 22:30:25 -0700

    upload

--- liri.js
+++ liri.js
@@ -21,10 +21,36 @@ if (command === 'my-tweets'){
     }
 }
 else if (command === 'spotify-this-song'){
-    console.log("spotify");
+    var Spotify = require('node-spotify-api');
+    
+   var spotify = new Spotify({
+     id: 'b06564ce29134615878ce01935708487',
+     secret: '17f59d0695044c9597e43c72fd67a074'
+   });
+    
+   spotify
+   .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
+   .then(function(data) {
+     console.log(data); 
+   })
+   .catch(function(err) {
+     console.error('Error occurred: ' + err); 
+   });
+ 
 }
 else if (command === 'movie-this'){
-    console.log("movies");
+   // Include the request npm package (Don't forget to run "npm install request" in this folder first!)
+var request = require("request");
+
+// Then run a request to the OMDB API with the movie specified
+request("http://www.omdbapi.com/?t="+ process.argv[3]+"&y=&plot=short&apikey=trilogy", function(error, response, body) {
+
+  // If the request is successful (i.e. if the response status code is 200)
+  if (!error && response.statusCode === 200) {
+
+    console.log(response);
+  }
+});
 }
 else if (command === 'do-what-it-says'){
     console.log("???");
